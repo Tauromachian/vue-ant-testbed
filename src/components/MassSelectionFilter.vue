094d0fd9f02ef07c1be306f96a9f42b965eea104
Jose Garcia <jogarcia@tuta.io>
2022-05-16 18:07:20 -0400

    Create basic MassSelectionFilter

---
+++ src/components/MassSelectionFilter.vue
@@ -0,0 +1,33 @@
+<template>
+  <modal-filter>
+    <a-form>
+      <a-auto-complete
+        :options="options"
+        style="width: 200px"
+        placeholder="input here"
+        @select="onSelect"
+        @search="onSearch"
+      />
+    </a-form>
+  </modal-filter>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+
+import ModalFilter from "./ModalFilter.vue";
+
+import { FormMassSelectionFilter } from "@/types/formMassSelectionFilter";
+
+export default Vue.extend({
+  components: { ModalFilter },
+  props: {
+    value: {
+      type: Object as PropType<FormMassSelectionFilter>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style></style>
