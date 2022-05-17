742eb8df53b3bd11ca317639493576e3ad6a2a59
Jose Garcia <jogarcia@tuta.io>
2022-05-17 17:57:11 -0400

    Use form-item for the labels

--- src/components/MassSelectionFilter.vue
+++ src/components/MassSelectionFilter.vue
@@ -1,66 +1,76 @@
 <template>
-  <modal-filter>
+  <modal-filter v-model="opened" vertical>
     <a-form>
-      <label for="">Nested Groups</label>
-      <a-auto-complete
-        v-model="form.nestedGroup"
-        :options="nestedGroups"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Device</label>
-      <a-auto-complete
-        v-model="form.device"
-        :options="devices"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Camera</label>
-      <a-auto-complete
-        v-model="form.camera"
-        :options="cameras"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Project</label>
-      <a-auto-complete
-        v-model="form.project"
-        :options="projects"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Job</label>
-      <a-auto-complete
-        v-model="form.job"
-        :options="jobs"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Use Case</label>
-      <a-auto-complete
-        v-model="form.useCase"
-        :options="useCases"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Model</label>
-      <a-auto-complete
-        v-model="form.model"
-        :options="models"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Tags</label>
-      <a-auto-complete
-        v-model="form.tags"
-        :options="tags"
-        style="width: 200px"
-        placeholder="input here"
-      />
-      <label for="">Start Date</label>
-      <a-date-picker v-model="form.dateStart" />
-      <label for="">Finish Date</label>
-      <a-date-picker v-model="form.dateFinish" />
+      <a-form-item label="Nested Groups" name="nested-groups">
+        <a-auto-complete
+          v-model="form.nestedGroup"
+          :options="nestedGroups"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Device" name="device">
+        <a-auto-complete
+          v-model="form.device"
+          :options="devices"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Camera" name="camera">
+        <a-auto-complete
+          v-model="form.camera"
+          :options="cameras"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Project" name="project">
+        <a-auto-complete
+          v-model="form.project"
+          :options="projects"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Job" name="job">
+        <a-auto-complete
+          v-model="form.job"
+          :options="jobs"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Use Case" name="use-case">
+        <a-auto-complete
+          v-model="form.useCase"
+          :options="useCases"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Model" name="model">
+        <a-auto-complete
+          v-model="form.model"
+          :options="models"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Tags" name="tags">
+        <a-auto-complete
+          v-model="form.tags"
+          :options="tags"
+          style="width: 200px"
+          placeholder="input here"
+        />
+      </a-form-item>
+      <a-form-item label="Start Date" name="start-date">
+        <a-date-picker v-model="form.dateStart" />
+      </a-form-item>
+      <a-form-item label="Finish Date" name="finish-date">
+        <a-date-picker v-model="form.dateFinish" />
+      </a-form-item>
     </a-form>
   </modal-filter>
 </template>
@@ -82,6 +92,7 @@ export default Vue.extend({
   },
   data() {
     return {
+      opened: false,
       nestedGroups: [],
       devices: [],
       cameras: [],
