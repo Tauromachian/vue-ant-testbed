81a60cda928241b1dc8ecdb83e6210c11616d774
Jose Garcia <jogarcia@tuta.io>
2022-05-17 18:29:50 -0400

    Create ActionButtons component

---
+++ src/components/ActionButtons.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <a-button :loading="disabled" text @click="cancelClick">
+      <span class="btnfont">{{ cancelButtonText }}</span>
+    </a-button>
+    <a-button
+      type="primary"
+      class="margin-left-8px"
+      :loading="disabled"
+      @click="acceptClick"
+    >
+      <span class="btnfont">{{ acceptButtonText }}</span>
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ActionButtons",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    cancelButtonText: {
+      type: String,
+      default: "Cancelar",
+    },
+    acceptButtonText: {
+      type: String,
+      default: "Aceptar",
+    },
+  },
+  methods: {
+    acceptClick() {
+      this.$emit("accept-click");
+    },
+    cancelClick() {
+      this.$emit("cancel-click");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.margin-left-8px {
+  margin-left: 8px;
+}
+</style>
