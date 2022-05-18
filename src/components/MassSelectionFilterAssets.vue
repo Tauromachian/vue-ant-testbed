a4ee2629d5c1056ea488eba20f2ab2e0a3f35ee2
Jose Garcia <jogarcia@tuta.io>
2022-05-18 15:46:09 -0400

    Manage responsivity

--- src/components/MassSelectionFilterAssets.vue
+++ src/components/MassSelectionFilterAssets.vue
@@ -1,6 +1,14 @@
 <template>
   <a-row>
-    <a-col :span="4" v-for="(asset, index) in assets" :key="index">
+    <a-col
+      :xs="24"
+      :sm="12"
+      :md="8"
+      :lg="6"
+      :xl="4"
+      v-for="(asset, index) in assets"
+      :key="index"
+    >
       <mass-selection-filter-asset-card
         :asset="asset"
       ></mass-selection-filter-asset-card>
@@ -29,6 +37,36 @@ export default Vue.extend({
   data() {
     return {
       assets: [
+        {
+          assetArchive:
+            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+          organization: 0,
+          groups: 0,
+          job: 0,
+          camera: 0,
+          model: 0,
+          annotation: 0,
+          s3_path: 0,
+          asset_type: "",
+          create_date: "",
+          tags: "",
+          is_bad: "",
+        },
+        {
+          assetArchive:
+            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+          organization: 0,
+          groups: 0,
+          job: 0,
+          camera: 0,
+          model: 0,
+          annotation: 0,
+          s3_path: 0,
+          asset_type: "",
+          create_date: "",
+          tags: "",
+          is_bad: "",
+        },
         {
           assetArchive:
             "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
