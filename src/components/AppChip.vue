89696892fd37465adbbbb650b36033c2ad435246
Jose Garcia <jogarcia@tuta.io>
2022-05-18 21:59:52 -0400

    Make chip component

---
+++ src/components/AppChip.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="chip">{{ text }}</span>
+</template>
+
+<script>
+export default {
+  name: "AppChip",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chip {
+  background: #e0e0e0;
+  border-radius: 16px;
+  font-size: 14px;
+  height: 32px;
+  padding: 0.2em 1em;
+}
+</style>
