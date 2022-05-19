cd1a1d49d532011b7eb6895b2014edc1f797f016
Jose Garcia <jogarcia@tuta.io>
2022-05-18 21:03:35 -0400

    Make asset type

---
+++ src/types/asset.ts
@@ -0,0 +1,14 @@
+export interface Asset {
+  assetArchive: number;
+  organization: number;
+  groups: number;
+  job: number;
+  camera: number;
+  model: number;
+  annotation: number;
+  s3_path: number;
+  asset_type: string;
+  create_date: string;
+  tags: string;
+  is_bad: string;
+}
