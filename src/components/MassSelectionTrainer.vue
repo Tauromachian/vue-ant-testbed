99c3943a9f860341d01b30614dfa2c6104131fc4
Jose Garcia <jogarcia@tuta.io>
2022-05-19 14:08:11 -0400

    Create basic MassSelectionTrainer component

---
+++ src/components/MassSelectionTrainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <mass-selection-filter-form
+    @click:accept="postTrainedAsset"
+  ></mass-selection-filter-form>
+</template>
+
+<script>
+import MassSelectionFilterForm from "./MassSelectionFilterForm.vue";
+
+export default {
+  name: "MassSelectionTrainer",
+  components: {
+    MassSelectionFilterForm,
+  },
+  methods: {
+    postTrainedAsset() {
+      return;
+    },
+  },
+};
+</script>
+
+<style></style>
