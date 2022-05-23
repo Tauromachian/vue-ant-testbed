236180bef7e97ab96b2e8fc180131f051b3140ce
Jose Garcia <jogarcia@tuta.io>
2022-05-23 09:01:52 -0400

    Setup stepper in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,55 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <mass-selection-filter></mass-selection-filter>
+    <div class="container">
+      <training-stepper></training-stepper>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import MassSelectionFilter from "@/components/MassSelectionFilter.vue";
+import TrainingStepper from "@/components/TrainingStepper.vue";
 
 export default Vue.extend({
-  components: { MassSelectionFilter },
+  components: { TrainingStepper },
 });
 </script>
+
+<style scoped>
+.container {
+  max-width: 100%;
+}
+
+@media screen and (min-width: 640px) {
+  .container {
+    max-width: 610px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .container {
+    max-width: 728px;
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .container {
+    max-width: 954px;
+  }
+}
+
+@media screen and (min-width: 1280px) {
+  .container {
+    max-width: 1220px;
+  }
+}
+
+@media screen and (min-width: 1536px) {
+  .container {
+    max-width: 1456px;
+  }
+}
+</style>
